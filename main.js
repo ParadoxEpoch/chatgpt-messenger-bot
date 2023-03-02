68dab4b480200ef2db59cbc8cee5afbd43588987
Tristan Gauci <tristan@maega.com.au>
2023-03-02 22:33:23 +1100

    Move listener port to env

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(3000);
+app.listen(process.env.port, () => console.log(`\n--- Server is live and listening on port ${process.env.port}! ---\n`));
 
 // Facebook Page Access Token
 let token = process.env.fbPageToken;
